e2ed380a21fe91bb9cafa53fb45a95cb1977ed7b
Aiden Lee <aiden.lee.jh@gmail.com>
2018-11-26 19:56:50 -0800

    fixed cors

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const routes = require('./routes');
 
 const app = express();
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
 app.configure(() => {
-  app.use(cors());
+  app.use(allowCrossDomain);
 });
 
 
